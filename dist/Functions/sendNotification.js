a97c91f50501434b36798884828adc0782ebe147
“Berenice <bherreran24@gmail.com>
2026-01-11 23:15:03 -0300

    createn env variables

--- dist/Functions/sendNotification.js
+++ dist/Functions/sendNotification.js
@@ -5,7 +5,9 @@ const NotificationManager_1 = require("../Application/NotificationManager");
 const SendGridMockProvider_1 = require("../Infrastructure/Providers/SendGridMockProvider");
 const TwilioMockProvider_1 = require("../Infrastructure/Providers/TwilioMockProvider");
 const InMemoryRateLimiter_1 = require("../Infrastructure/Services/InMemoryRateLimiter");
-const manager = new NotificationManager_1.NotificationManager([new SendGridMockProvider_1.SendGridMockProvider(), new TwilioMockProvider_1.TwilioMockProvider()], new InMemoryRateLimiter_1.InMemoryRateLimiter(2, 10));
+const rateLimitMax = Number(process.env.RATE_LIMIT_MAX ?? 2);
+const rateLimitWindow = Number(process.env.RATE_LIMIT_WINDOW ?? 10);
+const manager = new NotificationManager_1.NotificationManager([new SendGridMockProvider_1.SendGridMockProvider(), new TwilioMockProvider_1.TwilioMockProvider()], new InMemoryRateLimiter_1.InMemoryRateLimiter(rateLimitMax, rateLimitWindow));
 const handler = async (event) => {
     const body = JSON.parse(event.body);
     await manager.send({
@@ -15,7 +17,7 @@ const handler = async (event) => {
     });
     return {
         statusCode: 200,
-        body: JSON.stringify({ status: 'sent' }),
+        body: JSON.stringify({ status: "sent" }),
     };
 };
 exports.handler = handler;
--- dist/Infrastructure/Providers/SendGridMockProvider.js
+++ dist/Infrastructure/Providers/SendGridMockProvider.js
@@ -3,10 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.SendGridMockProvider = void 0;
 class SendGridMockProvider {
     async send(notification) {
-        if (Math.random() < 0.5) {
-            throw new Error('SendGrid failed');
+        const failureRate = Number(process.env.SENDGRID_FAILURE_RATE ?? 0.5);
+        if (Math.random() < failureRate) {
+            throw new Error("SendGrid failed");
         }
-        console.log('SendGrid sent:', notification.message);
+        console.log("SendGrid sent:", notification.message);
     }
 }
 exports.SendGridMockProvider = SendGridMockProvider;
